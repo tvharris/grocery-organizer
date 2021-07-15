5485e7f69bb6d36ef955b59d01eddff2163d7178
Travis Harris <travisvharris@gmail.com>
2021-07-15 03:04:52 +0000

    Add button to TestAPI

--- front/src/components/TestAPI.js
+++ front/src/components/TestAPI.js
@@ -1,41 +1,54 @@
+// Source for this AJAX code: https://reactjs.org/docs/faq-ajax.html
 import { useState, useEffect } from 'react'
 
 function TestAPI() {
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [item, setItem] = useState([]);
+  const [error, setError] = useState(null)
+  const [isLoaded, setIsLoaded] = useState(false)
+
+  // This doesn't have to be called 'item'
+  const [item, setItem] = useState([])
+
+  // Callback function for button click, updates the state, clearing item
+  const goodbyeWorld = () => {
+    setItem("")
+  }
 
   // Note: the empty deps array [] means
   // this useEffect will run once
   // similar to componentDidMount()
   useEffect(() => {
-    //fetch("http://localhost:5000/api")
-    //fetch("http://192.168.0.4:5000/api")
-    fetch("https://quiet-brook-53041.herokuapp.com/api")
-      .then(res => res.json())
+    // fetch("http://localhost:5000/api")
+    // fetch("http://192.168.0.4:5000/api")
+    fetch('https://quiet-brook-53041.herokuapp.com/api')
+      .then((res) => res.json())
       .then(
         (result) => {
-          setIsLoaded(true);
-          setItem(result);
+          setIsLoaded(true)
+          setItem(result)
         },
         // Note: it's important to handle errors here
         // instead of a catch() block so that we don't swallow
         // exceptions from actual bugs in components.
         (error) => {
-          setIsLoaded(true);
-          setError(error);
+          setIsLoaded(true)
+          setError(error)
         }
       )
   }, [])
 
   if (error) {
-    return <div>Error: {error.message}</div>;
+    return <div>Error: {error.message}</div>
   } else if (!isLoaded) {
-    return <div>Loading...</div>;
+    return <div>Loading...</div>
   } else {
     return (
-      <div>Response from backend: {item.hello}</div>
-    );
+      <div>
+        Response from backend: {item.hello}
+        <div>
+          <button onClick={goodbyeWorld}>Goodbye World!</button>
+        </div>
+      </div>
+    )
   }
 }
 
